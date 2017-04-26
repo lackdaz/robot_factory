9ab24697b724698443cbc54be7ad9330ff025eea
seth loh <haloandwings@gmail.com>
2017-04-26 11:40:12 +0800

    logic done

--- robot.rb
+++ robot.rb
@@ -1,6 +1,18 @@
+require 'pry'
+
 class Robot
-  attr_reader :name
-  def initialize(name = 'AB123')
-    @name = name
+  attr_accessor :name
+  def initialize
+    reset
+  end
+  def reset
+    num1 = ('a'..'z').to_a[rand(26)].to_s.upcase
+    num2 = ('a'..'z').to_a[rand(26)].to_s.upcase
+    num3 = rand(9).to_s
+    num4 = rand(9).to_s
+    num5 = rand(9).to_s
+    p @name = num1 + num2 + num3 + num4 + num5
   end
 end
+
+binding.pry
