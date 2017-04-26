54495b58e95ccdefbef55e7b54bce3091787a975
seth loh <haloandwings@gmail.com>
2017-04-26 10:26:40 +0800

    test

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,3 +1,12 @@
 # /^[A-z]+$/
 
-require'../robot'
+require '../robot'
+
+describe Robot do
+  describe 'Initialization' do
+    it 'should create new robot with name' do
+      r1 = Robot.new
+      expect(r1.name).to exist
+    end
+  end
+end
