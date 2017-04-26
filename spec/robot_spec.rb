04030b42c6c38c91c403419d2848bf22e8f253c2
seth loh <haloandwings@gmail.com>
2017-04-26 10:32:29 +0800

    added more comments

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,4 +1,10 @@
 # /^[A-z]+$/
+# Manage robot factory settings
+
+# When robots come off the factory floor, they have no name
+# The firs ttime you boot them up, a random name is generated in the format of two uppercase letters followed by three digits, such as RX837 or BC811.
+
+# Every 
 
 require_relative '../robot'
 
